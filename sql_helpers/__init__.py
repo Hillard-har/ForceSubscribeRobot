9f1aaedb52531d255730bdfac01a0764138601bd
Hillard Har <73195851+Hillard-har@users.noreply.github.com>
2021-01-14 10:35:00 +0530

    Update __init__.py

--- sql_helpers/__init__.py
+++ sql_helpers/__init__.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from Config import Config
+from sqlalchemy.dialects import registry
 
+...
+
+registry.register('mongodb', 'mongodb.sqlalchemy', 'dialect')
 
 def start() -> scoped_session:
     engine = create_engine(Config.DATABASE_URL)
